913a665bcb7bc682b2d351ae17f366b987a4f11f
Kevin Duffy <soadhlh@gmail.com>
2026-05-31 16:13:28 -0400

    db seed file
    Make sure db:seed is ran so there is always 1 post object to stop certain post when checking for post objects.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Post.find_or_create_by(title: "First Post") do |post|
+  post.body = "Hello and welcome to my site and my first post on my blog. This is more of a placeholder than anything else (kept in my db/seeds.rb file) but I also will keep the date consistent instead of using Time.now, so that I know the first day of my published blog!"
+  post.category = "blog"
+  post.slug = "first-post"
+  post.published_at = Time.parse("2026-05-31 15:58:52")
+end
