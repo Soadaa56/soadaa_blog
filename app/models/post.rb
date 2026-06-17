efc262301a68f2cfcf881e699b39324e9e512006
Kevin Duffy <soadhlh@gmail.com>
2026-06-17 11:02:10 -0400

    Cover image variants
    Using default active storage & libvips

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,21 @@ class Post < ApplicationRecord
 
     enum :category, { blog: 0, review: 1, impression: 2, ranking: 3, guide: 4 }
     scope :published, -> { where("published_at <= ?", Time.current) }
-    has_one_attached :cover_image
+    has_one_attached :cover_image do |attachable|
+      attachable.variant :desktop,
+        resize_to_limit: [ 1920, 1920 ],
+        format: :webP
+
+      attachable.variant :tablet,
+        resize_to_limit: [ 1280, 1280 ],
+        format: :webp,
+        saver: 90
+
+      attachable.variant :mobile,
+        resize_to_limit: [ 480, 480 ],
+        format: :webp,
+        saver: 80
+    end
     has_many_attached :images
     validates :title, presence: true
     validates :body, presence: true
