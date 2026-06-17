050c7a612f719a50d1d4a533b412b7e28d82b2f3
Kevin Duffy <soadhlh@gmail.com>
2026-06-17 17:42:54 -0400

    Updates page
    Minor code layout for css file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "feed", to: "posts#feed", defaults: { format: :atom }
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+  get "updates", to: "pages#updates"
 
   get "up" => "rails/health#show", as: :rails_health_check
 
